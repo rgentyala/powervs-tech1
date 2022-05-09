32239e31506d768563a60b4e487fb4ab8974303a
rgentyala <54944537+rgentyala@users.noreply.github.com>
2022-05-09 15:58:22 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,6 @@ data "ibm_pi_image" "ds_image" {
   pi_image_name        = var.image
   pi_cloud_instance_id = local.pid
 }
-locals {
-  public_image_name = "7200-05-01"
-  catalog_image = [for x in data.ibm_pi_catalog_images.catalog_images.images : x if x.name == local.public_image_name]
-}
 data "ibm_pi_network" "power_network" {
   pi_cloud_instance_id = local.pid
   pi_network_name      = var.network_name
