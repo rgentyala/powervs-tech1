6ebef4e43ec991e80c4cec72d17c0ec81b684717
rgentyala <54944537+rgentyala@users.noreply.github.com>
2022-05-09 14:12:04 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,17 @@ data "ibm_pi_image" "ds_image" {
   pi_image_name        = var.image
   pi_cloud_instance_id = local.pid
 }
+
 data "ibm_pi_network" "power_network" {
   pi_cloud_instance_id = local.pid
   pi_network_name      = var.network_name
 }
-  
+
+data "ibm_pi_storage_type_capacity" "type" {
+  pi_cloud_instance_id = local.pid
+  pi_storage_type = var.storage_tier
+}
+
 resource "ibm_pi_instance" "instance" {
   pi_cloud_instance_id = local.pid
   pi_memory            = var.memory
@@ -15,7 +21,7 @@ resource "ibm_pi_instance" "instance" {
   pi_proc_type         = var.processor_type
   pi_image_id          = data.ibm_pi_image.ds_image.id
   pi_sys_type          = var.sys_type
-  pi_storage_type      = var.storage_tier
+  pi_storage_type      = data.ibm_pi_storage_type_capacity.type.pi_storage_type
   pi_network {
     network_id = data.ibm_pi_network.power_network.id
   }
