f80ed462025623ad8e4b687ce8c79e6f93b1453f
rgentyala <54944537+rgentyala@users.noreply.github.com>
2022-05-09 13:58:33 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,11 @@
-data "ibm_pi_catalog_images" "catalog_images" {
-  sap                  = true
-  vtl                  = true
+data "ibm_pi_image" "ds_image" {
+  pi_image_name        = var.image
   pi_cloud_instance_id = local.pid
 }
 
 locals {
   public_image_name = "7200-05-01"
   catalog_image = [for x in data.ibm_pi_catalog_images.catalog_images.images : x if x.name == local.public_image_name]
-}
-
-data "ibm_pi_key" "key" {
-  pi_cloud_instance_id = local.pid
-  pi_key_name          = var.ssh_key_name
 }
 
 data "ibm_pi_network" "power_network" {
@@ -25,10 +19,10 @@ resource "ibm_pi_instance" "instance" {
   pi_processors        = var.processors
   pi_instance_name     = var.instance_name
   pi_proc_type         = var.processor_type
-  pi_image_id          = local.catalog_image[0].image_id
+  pi_image_id          = data.ibm_pi_image.ds_image.id
   pi_key_pair_name     = var.ssh_key_name
   pi_sys_type          = var.sys_type
-  pi_storage_type = var.storage_tier
+  pi_storage_type      = var.storage_tier
   pi_network {
     network_id = data.ibm_pi_network.power_network.id
   }
