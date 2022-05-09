8ba642f4429b17efcadc658ecb6f40c4c7b271ea
rgentyala <54944537+rgentyala@users.noreply.github.com>
2022-05-09 12:29:00 -0500

    Added modified files that work with Terraform

---
+++ main.tf
@@ -0,0 +1,29 @@
+data "ibm_pi_image" "ds_image" {
+  pi_image_name        = "IBMi-74-05-2984-1"
+  pi_cloud_instance_id = local.pid
+}
+
+data "ibm_pi_key" "key" {
+  pi_cloud_instance_id = local.pid
+  pi_key_name          = var.ssh_key_name
+}
+
+data "ibm_pi_network" "power_network" {
+  pi_cloud_instance_id = local.pid
+  pi_network_name      = var.network_name
+}
+
+resource "ibm_pi_instance" "instance" {
+  pi_cloud_instance_id = local.pid
+  pi_memory            = var.memory
+  pi_processors        = var.processors
+  pi_instance_name     = var.instance_name
+  pi_proc_type         = var.processor_type
+  pi_image_id          = local.catalog_image[0].image_id
+  pi_key_pair_name     = data.ds_image.id
+  pi_sys_type          = var.sys_type
+  pi_storage_type = var.storage_tier
+  pi_network {
+    network_id = data.ibm_pi_network.power_network.id
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,45 @@
+variable "crn" {
+  type        = string
+  default     = "crn:v1:staging:public:power-iaas:dal12:a/efe5e8b9d3f04b948790fe5499bd18bc:6021a723-bcab-4d3f-9985-d0cb2f864f35::"
+  description = "Power Systems Virtual Server CRN"
+}
+variable "memory" {
+  type        = number
+  default     = 4
+  description = "The amount of memory that you want to assign to your instance in gigabytes"
+}
+variable "processors" {
+  type        = number
+  default     = 0.25
+  description = "The number of vCPUs to assign to the VM as visible within the guest Operating System"
+}
+variable "instance_name" {
+  type        = string
+  default     = "estvm12345"
+  description = "The name to assign to the instance"
+}
+variable "processor_type" {
+  type        = string
+  default     = "shared"
+  description = "The type of processor mode in which the VM will run with 'shared', 'capped' or 'dedicated'"
+}
+variable "sys_type" {
+  type        = string
+  default     = "s922"
+  description = "The type of system on which to create the VM: 's922', 'e880', 'e980', 'e1080', or 's1022'"
+}
+variable "storage_tier" {
+  type        = string
+  default     = "tier3"
+  description = "The Storage Tier"
+}
+variable "ssh_key_name" {
+  type        = string
+  default     = "CHASE-TEST"
+  description = "The name of the public SSH RSA key to use when creating the instance, as defined for the selected Power Systems Virtual Server CRN"
+}
+variable "network_name" {
+  type        = string
+  default     = "rk-sn1"
+  description = "The network ID or name to assign to the instance, as defined for the selected Power Systems Virtual Server CRN"
+}
