5ee7598c026270bf1895cf4f34d1fbf86752fdf1
rgentyala <54944537+rgentyala@users.noreply.github.com>
2022-05-18 16:48:55 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,16 @@
 data "ibm_pi_key" "dskey" {
+  depends_on           = [ibm_pi_key.key]
   pi_cloud_instance_id = var.powerinstanceid
   pi_key_name          = var.sshkeyname
 }
-data "ibm_pi_image" "ds_image" {
-  pi_image_name        = var.image
+
+resource "ibm_pi_network" "power_networks" {
+  count                = 1
+  pi_network_name      = var.networkname
   pi_cloud_instance_id = var.powerinstanceid
+  pi_network_type      = "pub-vlan"
 }
-data "ibm_pi_network" "power_network" {
-  count                = 2
-  pi_cloud_instance_id = local.pid
-  pi_network_name      = var.network_name1
-  pi_network_name      = var.network_name2
-  }
+
 data "ibm_pi_public_network" "dsnetwork" {
   depends_on           = [ibm_pi_network.power_networks]
   pi_cloud_instance_id = var.powerinstanceid
@@ -35,16 +34,19 @@ data "ibm_pi_image" "powerimages" {
   pi_image_name        = var.imagename
   pi_cloud_instance_id = var.powerinstanceid
 }
-resource "ibm_pi_instance" "instance" {
-  pi_cloud_instance_id = var.powerinstanceid
-  pi_memory            = var.memory
-  pi_processors        = var.processors
-  pi_instance_name     = var.instance_name
-  pi_proc_type         = var.processor_type
-  pi_image_id          = data.ibm_pi_image.ds_image.id
+
+resource "ibm_pi_instance" "test-instance" {
+  pi_memory            = "4"
+  pi_processors        = "2"
+  pi_instance_name     = var.instancename
+  pi_proc_type         = "shared"
+  pi_image_id          = data.ibm_pi_image.powerimages.id
   pi_key_pair_name     = data.ibm_pi_key.dskey.id
-  pi_sys_type          = var.sys_type
+  pi_sys_type          = "s922"
+  pi_cloud_instance_id = var.powerinstanceid
+  pi_volume_ids        = [data.ibm_pi_volume.dsvolume.id]
+
   pi_network {
-    network_id = data.ibm_pi_network.power_network.id
+    network_id = data.ibm_pi_public_network.dsnetwork.id
   }
 }
