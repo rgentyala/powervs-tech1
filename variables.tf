cab0ce9ba0e0ba43bc16976edff678c3b545dd4d
rgentyala <54944537+rgentyala@users.noreply.github.com>
2022-05-18 16:50:12 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,48 +1,40 @@
-variable "crn" {
-  type        = string
-  description = "Power Systems Virtual Server CRN"
+variable "region" {
+  default = "lon"
 }
-variable "image" {
-  type        = string
-  default     = "IBMi-74-05-2984-1"
-  description = "image"
+
+variable "zone" {
+  default = "lon04"
 }
-variable "storage-type" {
-  type        = string
-  default     = "Tier3-Flash-1"
-  description = "Storage type defined for consuming a volume for OS"
+
+variable "imagename" {
+  description = "Name of the image to be used"
+  default     = "Linux-CentOS-8-3"
 }
-variable "storage-tier" {
-  type        = string
-  default     = "block"
-  description = "Storage type defined for consuming a volume for OS"
+
+variable "powerinstanceid" {
+  description = "Power Instance associated with the account"
 }
-variable "memory" {
-  type        = number
-  default     = 4
-  description = "The amount of memory that you want to assign to your instance in gigabytes"
+
+variable "instancename" {
+  default     = "myinstance"
+  description = "Name of the instance"
 }
-variable "processors" {
-  type        = number
-  default     = 0.25
-  description = "The number of vCPUs to assign to the VM as visible within the guest Operating System"
+
+variable "sshkeyname" {
+  default     = "mykey"
+  description = "Name of the ssh key to be used"
 }
-variable "instance_name" {
-  type        = string
-  default     = "my_test_vm"
-  description = "The name to assign to the instance"
+
+variable "volname" {
+  default     = "myvol"
+  description = "Name of the volume"
 }
-variable "processor_type" {
-  type        = string
-  default     = "shared"
-  description = "The type of processor mode in which the VM will run with 'shared', 'capped' or 'dedicated'"
+
+variable "networkname" {
+  default     = "mypublicnw"
+  description = "Name of the network"
 }
-variable "sys_type" {
-  type        = string
-  default     = "s922"
-  description = "The type of system on which to create the VM: 's922', 'e880', 'e980', 'e1080', or 's1022'"
-}
-variable "network_name" {
-  type        = string
-  description = "The network ID or name to assign to the instance, as defined for the selected Power Systems Virtual Server CRN"
+
+variable "sshkey" {
+  description = "Public ssh key"
 }
