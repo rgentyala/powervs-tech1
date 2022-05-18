ac06ad01392c6dcb7fb3350b6b57dacb28548286
rgentyala <54944537+rgentyala@users.noreply.github.com>
2022-05-18 16:42:58 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,17 @@
+data "ibm_pi_key" "dskey" {
+  pi_cloud_instance_id = local.pid
+  pi_key_name          = var.sshkeyname
+}
 data "ibm_pi_image" "ds_image" {
   pi_image_name        = var.image
   pi_cloud_instance_id = local.pid
 }
 data "ibm_pi_network" "power_network" {
+  count                = 2
   pi_cloud_instance_id = local.pid
-  pi_network_name      = var.network_name
-}
+  pi_network_name      = var.network_name1
+  pi_network_name      = var.network_name2
+  }
 resource "ibm_pi_instance" "instance" {
   pi_cloud_instance_id = local.pid
   pi_memory            = var.memory
@@ -13,6 +19,7 @@ resource "ibm_pi_instance" "instance" {
   pi_instance_name     = var.instance_name
   pi_proc_type         = var.processor_type
   pi_image_id          = data.ibm_pi_image.ds_image.id
+  pi_key_pair_name     = data.ibm_pi_key.dskey.id
   pi_sys_type          = var.sys_type
   pi_network {
     network_id = data.ibm_pi_network.power_network.id
