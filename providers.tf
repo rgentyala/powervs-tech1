0551f168a5a8ee303706814162db6f65db9d3a16
rgentyala <54944537+rgentyala@users.noreply.github.com>
2022-05-18 16:51:14 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,19 +1,9 @@
-terraform {
-  required_providers {
-    ibm = {
-      source = "IBM-Cloud/ibm"
-    }
-  }
-}
-
-locals {
-  pvs_info = split(":", var.crn)
-  location = local.pvs_info[5]
-  region   = can(regex("-", local.location)) ? (can(regex("-[0-9]+$", local.location)) ? replace(local.location, regex("-[0-9]+$", local.location), "") : local.location) : (can(regex("[0-9]+$", local.location)) ? replace(local.location, regex("[0-9]+$", local.location), "") : local.location)
-  pid      = local.pvs_info[7]
+variable "ibmcloud_api_key" {
+  description = "Enter your IBM Cloud API Key, you can get your IBM Cloud API key using: https://cloud.ibm.com/iam#/apikeys"
 }
 
 provider "ibm" {
-  region = local.region
-  zone   = local.location
+  ibmcloud_api_key = var.ibmcloud_api_key
+  region           = var.region
+  zone             = var.zone
 }
