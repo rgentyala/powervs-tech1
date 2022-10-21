3156a373192efc1af8b5c3d5507a27f93ef7285e
DennisFDD <93407709+DennisFDD@users.noreply.github.com>
2022-10-21 15:22:10 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,9 +8,9 @@ data "ibm_pi_network" "network" {
   pi_cloud_instance_id = "42175bd4-dc42-4ce0-ac6f-bc55caac4b7c"
 }
 
-data "ibm_pi_image" "powerimages" {
-  pi_image_name        = var.imagename
-  pi_cloud_instance_id = var.powerinstanceid
+data "ibm_pi_image" "power_image" {
+  pi_cloud_instance_id = local.cloud_instance_id
+  pi_image_name        = var.image_name
 }
 
 resource "ibm_pi_instance" "test-instance" {
@@ -19,7 +19,7 @@ resource "ibm_pi_instance" "test-instance" {
   pi_processors        = "0.25"
   pi_instance_name     = var.instancename
   pi_proc_type         = "shared"
-  pi_image_id          = var.ibm_pi_image.powerimages.id
+  pi_image_id          = data.ibm_pi_image.power_image.id
   pi_key_pair_name     = data.ibm_pi_key.ssh_key.id
   pi_sys_type          = "s922"
   pi_storage_type      = "tier3"
