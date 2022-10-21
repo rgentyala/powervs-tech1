0051b8d28eab614cadefe8304afc3fe4f7c44ea0
DennisFDD <93407709+DennisFDD@users.noreply.github.com>
2022-10-21 14:52:59 -0600

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -5,16 +5,3 @@ terraform {
     }
   }
 }
-
-locals {
-  pvs_info          = split(":", var.crn)
-  location          = local.pvs_info[5]
-  region            = can(regex("-", local.location)) ? (can(regex("-[0-9]+$", local.location)) ? replace(local.location, regex("-[0-9]+$", local.location), "") : local.location) : (can(regex("[0-9]+$", local.location)) ? replace(local.location, regex("[0-9]+$", local.location), "") : local.location)
-  cloud_instance_id = local.pvs_info[7]
-}
-
-provider "ibm" {
-  ibmcloud_api_key = var.ibm_cloud_api_key
-  region           = local.region
-  zone             = local.location
-}
