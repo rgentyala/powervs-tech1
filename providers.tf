9c34fae1dd225f2e6bb91b5f7feb0aecc71b284f
DennisFDD <93407709+DennisFDD@users.noreply.github.com>
2022-10-21 14:34:52 -0600

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -7,3 +7,18 @@ provider "ibm" {
   region           = var.region
   zone             = var.zone
 }
+
+terraform {
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+    }
+  }
+}
+
+locals {
+  pvs_info          = split(":", var.crn)
+  location          = local.pvs_info[5]
+  region            = can(regex("-", local.location)) ? (can(regex("-[0-9]+$", local.location)) ? replace(local.location, regex("-[0-9]+$", local.location), "") : local.location) : (can(regex("[0-9]+$", local.location)) ? replace(local.location, regex("[0-9]+$", local.location), "") : local.location)
+  cloud_instance_id = local.pvs_info[7]
+}
