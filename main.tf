cc08934f94a97a09ef32141e12fce97c6045531c
DennisFDD <93407709+DennisFDD@users.noreply.github.com>
2022-10-21 14:30:06 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,25 +8,13 @@ data "ibm_pi_network" "ds_network" {
   pi_cloud_instance_id = "42175bd4-dc42-4ce0-ac6f-bc55caac4b7c"
 }
 
-resource "ibm_pi_network" "power_networks" {
-  count                = 1
-  pi_network_name      = var.networkname
-  pi_cloud_instance_id = var.powerinstanceid
-  pi_network_type      = "pub-vlan"
-}
-
-data "ibm_pi_public_network" "dsnetwork" {
-  depends_on           = [ibm_pi_network.power_networks]
-  pi_cloud_instance_id = var.powerinstanceid
-}
-
-
 data "ibm_pi_image" "powerimages" {
   pi_image_name        = var.imagename
   pi_cloud_instance_id = var.powerinstanceid
 }
 
 resource "ibm_pi_instance" "test-instance" {
+  pi_cloud_instance_id = local.cloud_instance_id
   pi_memory            = "5"
   pi_processors        = "0.25"
   pi_instance_name     = var.instancename
@@ -34,10 +22,9 @@ resource "ibm_pi_instance" "test-instance" {
   pi_image_id          = var.ibm_pi_image.powerimages.id
   pi_key_pair_name     = local.ibm_pi_key.key.id
   pi_sys_type          = "s922"
-  pi_cloud_instance_id = var.powerinstanceid
   pi_storage_type      = "tier3"
 
   pi_network {
-    network_id = data.ibm_pi_public_network.dsnetwork.id
+    network_id = local.ibm_pi_network.network.id
   }
 }
