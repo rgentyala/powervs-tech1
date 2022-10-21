6f8819d00c3bea107b364d12ea19f9c963c9b250
DennisFDD <93407709+DennisFDD@users.noreply.github.com>
2022-10-21 14:33:17 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,36 +1,14 @@
-variable "region" {
-  default = "lon"
-}
-
-variable "zone" {
-  default = "lon04"
-}
-
 variable "imagename" {
   description = "Name of the image to be used"
-  default     = "paytester_v1"
+  default     = "clai-pivote"
 }
 
 variable "powerinstanceid" {
   description = "Power Instance associated with the account"
+  default     = "crn:v1:bluemix:public:power-iaas:us-south:a/8fa8c2d1e1d943ad862c1e1d0860ed79:42175bd4-dc42-4ce0-ac6f-bc55caac4b7c::"
 }
 
 variable "instancename" {
   default     = "pt_instance"
   description = "Name of the instance"
-}
-
-variable "sshkeyname" {
-  default     = "mykey"
-  description = "Name of the ssh key to be used"
-}
-
-variable "volname" {
-  default     = "myvol"
-  description = "Name of the volume"
-}
-
-variable "networkname" {
-  default     = "mypublicnw"
-  description = "Name of the network"
 }
