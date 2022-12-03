d54030290f80335e4926556720260c4b583139d2
DennisFDD <93407709+DennisFDD@users.noreply.github.com>
2022-12-03 14:56:14 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ data "ibm_pi_key" "ssh_key" {
   pi_cloud_instance_id = "42175bd4-dc42-4ce0-ac6f-bc55caac4b7c"
 }
 
-
 data "ibm_pi_network" "network2" {
   pi_network_name = "testvpn"
   pi_cloud_instance_id = "42175bd4-dc42-4ce0-ac6f-bc55caac4b7c"
@@ -29,7 +28,31 @@ resource "ibm_pi_instance" "test-instance" {
     network_id  = data.ibm_pi_network.network2.id
   }
 
-      resource "ibm_pi_vpn_connection" "vpndfd" {
+      resource "ibm_pi_ike_policy" "example" {
+        pi_cloud_instance_id    = "42175bd4-dc42-4ce0-ac6f-bc55caac4b7c"
+        pi_policy_name          = "ikepol2"
+        pi_policy_dh_group = 20
+        pi_policy_encryption = "aes-256-cbc"
+        pi_policy_key_lifetime = 28800
+        pi_policy_preshared_key = "falabella"
+        pi_policy_version = 2
+        pi_policy_authentication = "sha-256"
+    }  
+  
+      resource "ibm_pi_ipsec_policy" "example" {
+        pi_cloud_instance_id    = "42175bd4-dc42-4ce0-ac6f-bc55caac4b7c"
+        pi_policy_name          = "ipsecpol2"
+        pi_policy_dh_group = 20
+        pi_policy_encryption = "aes-256-cbc"
+        pi_policy_key_lifetime = 28800
+        pi_policy_pfs = true
+        pi_policy_authentication = "hmac-sha-256-128"
+    }
+  
+  
+  
+  
+  resource "ibm_pi_vpn_connection" "vpndfd" {
         pi_cloud_instance_id    = "42175bd4-dc42-4ce0-ac6f-bc55caac4b7c"
         pi_vpn_connection_name  = "vpndfd"
         pi_ike_policy_id        = ibm_pi_ike_policy.policy.policy_id
