41c7e064b8f6415a0f9b67f16b4d061a0fd54090
DennisFDD <93407709+DennisFDD@users.noreply.github.com>
2022-12-05 10:39:02 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-resource "ibm_pi_ike_policy" "ikepol" {    
+resource "ibm_pi_ike_policy" "ikepol5" {    
   pi_cloud_instance_id    = "42175bd4-dc42-4ce0-ac6f-bc55caac4b7c"
-  pi_policy_name          = "ikepol4"    
+  pi_policy_name          = "ikepol5"    
   pi_policy_dh_group = 20   
   pi_policy_encryption = "aes-256-cbc"  
   pi_policy_key_lifetime = 28800    
@@ -9,9 +9,9 @@ resource "ibm_pi_ike_policy" "ikepol" {
   pi_policy_authentication = "sha-256"
 }  
 
-resource "ibm_pi_ipsec_policy" "ipsecpol" {
+resource "ibm_pi_ipsec_policy" "ipsecpol5" {
   pi_cloud_instance_id    = "42175bd4-dc42-4ce0-ac6f-bc55caac4b7c"
-  pi_policy_name          = "ipsecpol4"
+  pi_policy_name          = "ipsecpol5"
   pi_policy_dh_group = 20
   pi_policy_encryption = "aes-256-cbc"
   pi_policy_key_lifetime = 28800
@@ -22,8 +22,8 @@ resource "ibm_pi_ipsec_policy" "ipsecpol" {
 resource "ibm_pi_vpn_connection" "vpndfd2" {
   pi_cloud_instance_id    = "42175bd4-dc42-4ce0-ac6f-bc55caac4b7c"  
   pi_vpn_connection_name  = "vpndfd2"
-  pi_ike_policy_id        = "ikepol4"
-  pi_ipsec_policy_id      = "ipsecpol4"
+  pi_ike_policy_id        = "ikepol5"
+  pi_ipsec_policy_id      = "ipsecpol5"
   pi_vpn_connection_mode  = "route"
   pi_networks             = ["testvpn"]
   pi_peer_gateway_address = "169.46.19.238"
