2b0b9c7731c392a694020a1d8e809f9f9e286b9d
DennisFDD <93407709+DennisFDD@users.noreply.github.com>
2022-12-05 11:21:11 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@
 resource "ibm_pi_vpn_connection" "vpndfd2" {
   pi_cloud_instance_id    = "42175bd4-dc42-4ce0-ac6f-bc55caac4b7c"  
   pi_vpn_connection_name  = "vpndfd2"
-  pi_ike_policy_id        = "ikepol5"
-  pi_ipsec_policy_id      = "ipsecpol5"
+  pi_ike_policy_id        = "ikepol4"
+  pi_ipsec_policy_id      = "ipsecpol4"
   pi_vpn_connection_mode  = "route"
   pi_networks             = ["testvpn"]
   pi_peer_gateway_address = "169.46.19.238"
