59ab29abfab2ad34d4ff12fbc625b42412ae9925
DennisFDD <93407709+DennisFDD@users.noreply.github.com>
2022-12-05 10:24:34 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,6 @@
-data "ibm_pi_key" "ssh_key" {
-  pi_key_name          = "ssh-poc"
-  pi_cloud_instance_id = "42175bd4-dc42-4ce0-ac6f-bc55caac4b7c"
-}
-
-data "ibm_pi_network" "network2" {
-  pi_network_name = "testvpn"
-  pi_cloud_instance_id = "42175bd4-dc42-4ce0-ac6f-bc55caac4b7c"
-}
-
-data "ibm_pi_image" "power_image" {
-  pi_cloud_instance_id = local.cloud_instance_id
-  pi_image_name        = var.image_name
-}
-
 resource "ibm_pi_ike_policy" "ikepol" {    
   pi_cloud_instance_id    = "42175bd4-dc42-4ce0-ac6f-bc55caac4b7c"
-  pi_policy_name          = "ikepol3"    
+  pi_policy_name          = "ikepol4"    
   pi_policy_dh_group = 20   
   pi_policy_encryption = "aes-256-cbc"  
   pi_policy_key_lifetime = 28800    
@@ -26,7 +11,7 @@ resource "ibm_pi_ike_policy" "ikepol" {
 
 resource "ibm_pi_ipsec_policy" "ipsecpol" {
   pi_cloud_instance_id    = "42175bd4-dc42-4ce0-ac6f-bc55caac4b7c"
-  pi_policy_name          = "ipsecpol3"
+  pi_policy_name          = "ipsecpol4"
   pi_policy_dh_group = 20
   pi_policy_encryption = "aes-256-cbc"
   pi_policy_key_lifetime = 28800
@@ -37,28 +22,11 @@ resource "ibm_pi_ipsec_policy" "ipsecpol" {
 resource "ibm_pi_vpn_connection" "vpndfd2" {
   pi_cloud_instance_id    = "42175bd4-dc42-4ce0-ac6f-bc55caac4b7c"  
   pi_vpn_connection_name  = "vpndfd2"
-  pi_ike_policy_id        = "ikepol3"
-  pi_ipsec_policy_id      = "ipsecpol3"
+  pi_ike_policy_id        = "ikepol4"
+  pi_ipsec_policy_id      = "ipsecpol4"
   pi_vpn_connection_mode  = "route"
   pi_networks             = ["testvpn"]
   pi_peer_gateway_address = "169.46.19.238"
   pi_peer_subnets         = ["10.177.131.192/26"]
-}
-
-
-resource "ibm_pi_instance" "test-instance2" {
-  pi_cloud_instance_id = local.cloud_instance_id
-  pi_memory            = "5"
-  pi_processors        = "0.25"
-  pi_instance_name     = var.instancename
-  pi_proc_type         = "shared"
-  pi_image_id          = data.ibm_pi_image.power_image.id
-  pi_key_pair_name     = data.ibm_pi_key.ssh_key.id
-  pi_sys_type          = "s922"
-  pi_storage_type      = "tier3"
-
-    pi_network {
-    network_id  = data.ibm_pi_network.network2.id
-  }
 }
 
