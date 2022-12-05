e6dba3d810910a481ac55f381dcf12df628e6dee
DennisFDD <93407709+DennisFDD@users.noreply.github.com>
2022-12-05 17:09:00 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,18 +19,13 @@ resource "ibm_pi_ipsec_policy" "ipsecpol8" {
   pi_policy_authentication = "hmac-sha-256-128"
 }
 
-data "ibm_pi_network" "network2" {
-  pi_network_name = "testvpn"
-  pi_cloud_instance_id = "42175bd4-dc42-4ce0-ac6f-bc55caac4b7c"
-}
-
 resource "ibm_pi_vpn_connection" "vpndfd2" {
   pi_cloud_instance_id    = "42175bd4-dc42-4ce0-ac6f-bc55caac4b7c"  
   pi_vpn_connection_name  = "vpndfd2"
   pi_ike_policy_id        = ibm_pi_ike_policy.ikepol8.policy_id
   pi_ipsec_policy_id      = ibm_pi_ipsec_policy.ipsecpol8.policy_id
   pi_vpn_connection_mode  = "route"
-  pi_networks             = [data.ibm_pi_network.network2.id]
+  pi_networks             = ["testvpn"]
   pi_peer_gateway_address = "169.46.19.238"
   pi_peer_subnets         = ["10.177.131.192/26"]
 }
