ef8f223ddb187856b70066e9c4363994f99df72c
DennisFDD <93407709+DennisFDD@users.noreply.github.com>
2022-12-05 07:53:13 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,40 @@ data "ibm_pi_image" "power_image" {
   pi_image_name        = var.image_name
 }
 
-resource "ibm_pi_instance" "test-instance" {
+resource "ibm_pi_ike_policy" "ikepol" {    
+  pi_cloud_instance_id    = "42175bd4-dc42-4ce0-ac6f-bc55caac4b7c"
+  pi_policy_name          = "ikepol3"    
+  pi_policy_dh_group = 20   
+  pi_policy_encryption = "aes-256-cbc"  
+  pi_policy_key_lifetime = 28800    
+  pi_policy_preshared_key = "falabella"    
+  pi_policy_version = 2     
+  pi_policy_authentication = "sha-256"
+}  
+
+resource "ibm_pi_ipsec_policy" "ipsecpol" {
+  pi_cloud_instance_id    = "42175bd4-dc42-4ce0-ac6f-bc55caac4b7c"
+  pi_policy_name          = "ipsecpol3"
+  pi_policy_dh_group = 20
+  pi_policy_encryption = "aes-256-cbc"
+  pi_policy_key_lifetime = 28800
+  pi_policy_pfs = true
+  pi_policy_authentication = "hmac-sha-256-128"
+}
+
+resource "ibm_pi_vpn_connection" "vpndfd" {
+  pi_cloud_instance_id    = "42175bd4-dc42-4ce0-ac6f-bc55caac4b7c"  
+  pi_vpn_connection_name  = "vpndfd2"
+  pi_ike_policy_id        = "ikepol3"
+  pi_ipsec_policy_id      = "ipsecpol3"
+  pi_vpn_connection_mode  = "route"
+  pi_networks             = ["testvpn"]
+  pi_peer_gateway_address = "169.46.19.238"
+  pi_peer_subnets         = ["10.177.131.192/26"]
+}
+
+
+resource "ibm_pi_instance" "test-instance2" {
   pi_cloud_instance_id = local.cloud_instance_id
   pi_memory            = "5"
   pi_processors        = "0.25"
@@ -28,35 +61,4 @@ resource "ibm_pi_instance" "test-instance" {
     network_id  = data.ibm_pi_network.network2.id
   }
 }
-      resource "ibm_pi_ike_policy" "ikepol" {
-        pi_cloud_instance_id    = "42175bd4-dc42-4ce0-ac6f-bc55caac4b7c"
-        pi_policy_name          = "ikepol2"
-        pi_policy_dh_group = 20
-        pi_policy_encryption = "aes-256-cbc"
-        pi_policy_key_lifetime = 28800
-        pi_policy_preshared_key = "falabella"
-        pi_policy_version = 2
-        pi_policy_authentication = "sha-256"
-    }  
-  
-      resource "ibm_pi_ipsec_policy" "ipsecpol" {
-        pi_cloud_instance_id    = "42175bd4-dc42-4ce0-ac6f-bc55caac4b7c"
-        pi_policy_name          = "ipsecpol2"
-        pi_policy_dh_group = 20
-        pi_policy_encryption = "aes-256-cbc"
-        pi_policy_key_lifetime = 28800
-        pi_policy_pfs = true
-        pi_policy_authentication = "hmac-sha-256-128"
-    }
-   
-  resource "ibm_pi_vpn_connection" "vpndfd" {
-        pi_cloud_instance_id    = "42175bd4-dc42-4ce0-ac6f-bc55caac4b7c"
-        pi_vpn_connection_name  = "vpndfd"
-        pi_ike_policy_id        = "ikepol2"
-        pi_ipsec_policy_id      = "ipsecpol2"
-        pi_vpn_connection_mode  = "route"
-        pi_networks             = ["testvpn"]
-        pi_peer_gateway_address = "169.46.19.238"
-        pi_peer_subnets         = ["10.177.131.192/26"]
-    }
-  
+
