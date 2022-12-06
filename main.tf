f2a3dd6cd5dba5630dcfbab1392893137397c13d
DennisFDD <93407709+DennisFDD@users.noreply.github.com>
2022-12-06 16:47:39 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,3 +37,29 @@ resource "ibm_pi_vpn_connection" "rs_vpn_connection" {
         pi_peer_subnets         = ["10.177.131.192/26"]
     }
 
+data "ibm_pi_key" "ssh_key" {
+  pi_key_name          = "ssh-poc"
+  pi_cloud_instance_id = "42175bd4-dc42-4ce0-ac6f-bc55caac4b7c"
+}
+
+data "ibm_pi_image" "power_image" {
+  pi_cloud_instance_id = local.cloud_instance_id
+  pi_image_name        = var.image_name
+}
+
+resource "ibm_pi_instance" "test-instance2" {
+  pi_cloud_instance_id = local.cloud_instance_id
+  pi_memory            = "5"
+  pi_processors        = "0.25"
+  pi_instance_name     = var.instancename
+  pi_proc_type         = "shared"
+  pi_image_id          = data.ibm_pi_image.power_image.id
+  pi_key_pair_name     = data.ibm_pi_key.ssh_key.id
+  pi_sys_type          = "s922"
+  pi_storage_type      = "tier3"
+
+    pi_network {
+    network_id  = data.ibm_pi_network.ds_network.id
+  }
+}
+
