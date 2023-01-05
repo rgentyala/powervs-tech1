16575a0413dc727d5e821f91dbcff7549b4e3693
rgentyala <54944537+rgentyala@users.noreply.github.com>
2023-01-05 17:35:35 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,20 +1,20 @@
 variable "ibm_cloud_api_key" {
   type        = string
-  description = "API Key"
+  description = "API Key for clip account for user#1"
 }
 
 variable "image_name" {
-  description = "Name of the image to be used"
-  default     = "clai-pivote"
+  description = "Name of the image to be deployed"
+  default     = "cliptestimage"
 }
 
 variable "crn" {
   type        = string
-  default     = "crn:v1:bluemix:public:power-iaas:us-south:a/8fa8c2d1e1d943ad862c1e1d0860ed79:42175bd4-dc42-4ce0-ac6f-bc55caac4b7c::"
-  description = "Power Systems Virtual Server CRN"
+  default     = "crn:v1:bluemix:public:power-iaas:us-west:a/<name1>:<name2>::"
+  description = "Power Systems Virtual Server CRN reference"
 }
 
 variable "instancename" {
-  default     = "newvsi"
-  description = "Name of the instance"
+  default     = "newpowervs"
+  description = "Name of the instance to be created"
 }
