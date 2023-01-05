fd6ad0d5cfe6fcdf240759ec70722fc152463360
rgentyala <54944537+rgentyala@users.noreply.github.com>
2023-01-05 17:45:48 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "ibm_pi_ike_policy" "poc_ike_policy" {
         pi_policy_dh_group = 20
         pi_policy_encryption = "aes-256-cbc"
         pi_policy_key_lifetime = 28800
-        pi_policy_preshared_key = "falabella"
+        pi_policy_preshared_key = "cliptech"
         pi_policy_version = 2
         pi_policy_authentication = "sha-256"
     }
@@ -21,7 +21,7 @@ resource "ibm_pi_ipsec_policy" "poc_ipsec_policy" {
     }
 
 data "ibm_pi_network" "ds_network" {
-  pi_network_name = "testvpn"
+  pi_network_name = "vpntests"
   pi_cloud_instance_id = local.cloud_instance_id
     }
 
@@ -33,13 +33,13 @@ resource "ibm_pi_vpn_connection" "rs_vpn_connection" {
         pi_ipsec_policy_id      = ibm_pi_ipsec_policy.poc_ipsec_policy.policy_id
         pi_vpn_connection_mode  = "route"
         pi_networks             = [data.ibm_pi_network.ds_network.id]
-        pi_peer_gateway_address = "169.46.19.238"
-        pi_peer_subnets         = ["10.177.131.192/26"]
+        pi_peer_gateway_address = "x.x.x.x"
+        pi_peer_subnets         = ["y.y.y.y/26"]
     }
 
 data "ibm_pi_key" "ssh_key" {
   pi_key_name          = "ssh-poc"
-  pi_cloud_instance_id = "42175bd4-dc42-4ce0-ac6f-bc55caac4b7c"
+  pi_cloud_instance_id = "4xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxx"
 }
 
 data "ibm_pi_image" "power_image" {
@@ -47,9 +47,9 @@ data "ibm_pi_image" "power_image" {
   pi_image_name        = var.image_name
 }
 
-resource "ibm_pi_instance" "test-instance2" {
+resource "ibm_pi_instance" "test-instance1" {
   pi_cloud_instance_id = local.cloud_instance_id
-  pi_memory            = "5"
+  pi_memory            = "4"
   pi_processors        = "0.25"
   pi_instance_name     = var.instancename
   pi_proc_type         = "shared"
